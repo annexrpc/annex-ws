04f083d42624107d65a40341a6bbdd2256c31a0b
camshaft <bytheway.cameron@gmail.com>
2015-01-23 15:40:31 -0700

    use new decoder format

--- src/annex_ws.erl
+++ src/annex_ws.erl
@@ -60,7 +60,7 @@ init(Opts, _State) ->
     undefined ->
       exit(missing_marshal);
     Marshal ->
-      {ok, #state{marshal = Marshal}}
+      {ok, #state{marshal = Marshal}, 10000}
   end.
 
 websocket_handle({pong, _}, _ConnState, State) ->
@@ -68,9 +68,7 @@ websocket_handle({pong, _}, _ConnState, State) ->
 websocket_handle({binary, Bin}, _, State = #state{marshal = Marshal}) ->
   case catch Marshal:decode(Bin) of
     {ok, MsgID, Res} ->
-      handle_response(MsgID, {ok, Res}, State);
-    {error, MsgID, Res} ->
-      handle_response(MsgID, {error, Res}, State);
+      handle_message(MsgID, Res, State);
     Error ->
       error_logger:error_msg("Unable to decode response ~p~n~p~n", [Bin, Error]),
       {ok, State}
@@ -83,15 +81,40 @@ websocket_info(start, _ConnState, State) ->
   {ok, State};
 websocket_info({call, Module, Function, Arguments, Sender, Ref}, _, State = #state{marshal = Marshal, msg_id = MsgID, requests = Reqs}) ->
   %% TODO save sender and ref under msgid
-  Out = Marshal:encode(MsgID, call, Module, Function, Arguments),
-  {reply, {binary, Out}, State#state{requests = maps:put(MsgID, {Sender, Ref}, Reqs), msg_id = (MsgID + 1) rem ?MAX_NUM}}.
+  Out = Marshal:call(MsgID, [Module, Function], Arguments),
+
+  State2 = State#state{
+    requests = maps:put(MsgID, {Sender, Ref}, Reqs),
+    msg_id = (MsgID + 1) rem ?MAX_NUM
+  },
+
+  {reply, {binary, Out}, State2}.
 
 websocket_terminate(Reason, _ConnState, State) ->
   io:format("Websocket closed in state ~p wih reason ~p~n",
             [State, Reason]),
   ok.
 
-handle_response(MsgID, Msg, State = #state{requests = Reqs}) ->
+%% TODO implement the calling... not sure how that would work atm
+handle_message(_, {call, _, _}, State) ->
+  {ok, State};
+handle_message(_, {call, _, _, _}, State) ->
+  {ok, State};
+handle_message(_, {cast, _, _}, State) ->
+  {ok, State};
+handle_message(_, {cast, _, _, _}, State) ->
+  {ok, State};
+
+handle_message(MsgID, {response, Response}, State) ->
+  respond(MsgID, {ok, Response}, State);
+handle_message(MsgID, {response, Response, _Meta}, State) ->
+  respond(MsgID, {ok, Response}, State);
+handle_message(MsgID, {error, Code, Error}, State) ->
+  respond(MsgID, {error, {Code, Error}}, State);
+handle_message(MsgID, {error, Code, Error, _Meta}, State) ->
+  respond(MsgID, {error, {Code, Error}}, State).
+
+respond(MsgID, Msg, State = #state{requests = Reqs}) ->
   case maps:find(MsgID, Reqs) of
     {ok, {Sender, Ref}} ->
       Sender ! {Ref, Msg},
